f1ff88aa71efbd04fafd34e4f1b8a873400c93ec
parambirla <104878929+parambirla@users.noreply.github.com>
2024-11-26 01:10:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def api():
     global conversation_history
 
     #personality = Conscientiousness
-    message = request.json.get("message") + ' . (Respond to this assuming "Conscientiousness" personality trait without revealing "Conscientiousness" statement in response and give response for minimum 200 words) '
+    message = request.json.get("message") + ' . (Respond to this assuming "Conscientiousness" personality trait without revealing "Conscientiousness" statement in response) '
 
     conversation_history.append({"role": "user", "content": message})
     # Send the message to OpenAI's API and receive the response
